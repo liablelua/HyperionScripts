527b44a70e09a42c82d93d41365f2778eadb49a8
4fr <140775902+liablelua@users.noreply.github.com>
2024-08-18 00:46:56 -0500

    File API

--- HyperionUNC.lua
+++ HyperionUNC.lua
@@ -31,6 +31,81 @@ isfile = function(path)
     if File.Body == "1" then return true else return false end
 end
 
+makefolder = function(path)
+    local ask = "makefolder,"..path
+    local File = request({
+        Url = server,
+        Method = "POST",
+        Body = ask
+    })
+end
+
+isfolder = function(path)
+    local ask = "isfolder,"..path
+    local File = request({
+        Url = server,
+        Method = "POST",
+        Body = ask
+    })
+    if File.Body == "1" then return true else return false end
+end
+
+readfile = function(path)
+    local ask = "readfile,"..path
+    local File = request({
+        Url = server,
+        Method = "POST",
+        Body = ask
+    })
+    return File.Body
+end
+
+appendfile = function(path, content)
+    local ask = "appendfile,"..path..","..content
+    request({
+        Url = server,
+        Method = "POST",
+        Body = ask
+    })
+end
+
+deletefolder = function(path)
+    local ask = "deletefolder,"..path
+    request({
+        Url = server,
+        Method = "POST",
+        Body = ask
+    })
+end
+
+delfolder = deletefolder
+
+listfiles = function(path)
+    local ask = "listfiles,"..path
+    local Request = request({
+        Url = server,
+        Method = "POST",
+        Body = ask
+    }).Body
+    local table1 = {}
+    local Files = string.split(Request, ",")
+    for i, v in pairs(Files) do
+        table.insert(table1, v)
+    end
+    return table1
+end
+
+loadfile = function(path)
+    local ask = "loadfile,"..path
+    local Request = request({
+        Url = server,
+        Method = "POST",
+        Body = ask
+    }).Body
+    local Split = string.split(Request, ",")
+    return {Split[1], Split[2]}
+end
+
 function HttpGet(string) -- temporary
     return request({
         Url = string,
